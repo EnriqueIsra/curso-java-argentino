7aca55c89b8e006463b19601641b6be7ce60b466
EnriqueIsra <sirilo99x3@gmail.com>
2024-01-24 12:32:26 -0600

    Actualizacion Acer 252

--- IdeaProjects/colecciones/src/org/eitorresmendoza/ejemplos/map/EjemploHashMap.java
+++ IdeaProjects/colecciones/src/org/eitorresmendoza/ejemplos/map/EjemploHashMap.java
@@ -1,7 +1,9 @@
 package org.eitorresmendoza.ejemplos.map;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class EjemploHashMap {
     public static void main(String[] args) {
@@ -22,5 +24,46 @@ public class EjemploHashMap {
         System.out.println("nombre = " + nombre);
         String apellido = persona.get("apellido");
         System.out.println("apellido = " + apellido);
+
+        // String apellidoPaterno = persona.remove("apellido Paterno");
+        boolean borrado = persona.remove("apellido Paterno", "Doe");
+        System.out.println("borrado = " + borrado);
+        System.out.println("persona = " + persona);
+
+        boolean contieneLlave = persona.containsKey("email");
+        System.out.println("contieneLlave \"email\" = " + contieneLlave);
+        contieneLlave = persona.containsKey("apellido Paterno");
+        System.out.println("contieneLlave \"apellido Paterno\" = " + contieneLlave);
+
+        // Iterando el Map
+        System.out.println("*********************** utilizando values");
+        Collection<String> valores = persona.values();
+        for (String v: valores){
+            System.out.println("v = " + v);
+        }
+        System.out.println("*********************** KeySet");
+        Set<String> keys = persona.keySet();
+        for (String key: keys){
+            System.out.println("key = " + key);
+        }
+        System.out.println("*********************** entrySet");
+        for (Map.Entry<String, String> valorPar: persona.entrySet()){
+            System.out.println(valorPar.getKey() + " => " + valorPar.getValue());
+        }
+        System.out.println("*********************** KeySet");
+        for (String key: persona.keySet()){
+            String valor = persona.get(key);
+            System.out.println(key + " => " + valor);
+        }
+        System.out.println("*********************** forEach");
+        persona.forEach((key, valor) -> {
+            System.out.println(key + " => " + valor);
+        });
+        System.out.println("*********************** más métodos importantes");
+        System.out.println("total: " + persona.size());
+        System.out.println("contiene elementos = " + !persona.isEmpty());
+        boolean remplazado = persona.replace("nombre", "John", "El tranza");
+        System.out.println("remplazado = " + remplazado);
+        System.out.println("persona = " + persona);
     }
 }
