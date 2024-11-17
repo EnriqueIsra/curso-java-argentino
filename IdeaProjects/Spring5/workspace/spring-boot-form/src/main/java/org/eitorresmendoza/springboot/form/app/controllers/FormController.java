c54021999acab3215a81367695ee0f5422fec370
EnriqueIsra <sirilo99x3@gmail.com>
2024-11-17 14:32:09 -0600

    formateo de fechas con @InitBinder y registrando un CustomDateEditor parte 2, aplicando validador a atributos en particular y cambio a true para aplicar @NotNull

--- IdeaProjects/Spring5/workspace/spring-boot-form/src/main/java/org/eitorresmendoza/springboot/form/app/controllers/FormController.java
+++ IdeaProjects/Spring5/workspace/spring-boot-form/src/main/java/org/eitorresmendoza/springboot/form/app/controllers/FormController.java
@@ -33,7 +33,7 @@ public class FormController {
 		
 		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 		dateFormat.setLenient(false);
-		binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, false));
+		binder.registerCustomEditor(Date.class, "fechaNacimiento", new CustomDateEditor(dateFormat, true)); // "fechaNacimiento" con este string indicamos especificamente el atributo de la clase que queremos aplicar esta validacion
 	}
 	@GetMapping("/form")
 	public String form(Model model) {
