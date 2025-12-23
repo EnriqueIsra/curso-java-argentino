8dbd3b6ac4983422b64a19349dbe750acc9105f6
EnriqueIsra <sirilo99x3@gmail.com>
2025-12-23 12:14:05 -0600

    647. El método subscribe y eventos onNext, onError y onComplete

--- IdeaProjects/spring/8-springboot-reactor/src/main/java/com/enrique/curso/java/springboot/reactor/app/SpringbootReactorApplication.java
+++ IdeaProjects/spring/8-springboot-reactor/src/main/java/com/enrique/curso/java/springboot/reactor/app/SpringbootReactorApplication.java
@@ -1,5 +1,7 @@
 package com.enrique.curso.java.springboot.reactor.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,13 +10,27 @@ import reactor.core.publisher.Flux;
 @SpringBootApplication
 public class SpringbootReactorApplication implements CommandLineRunner {
 
+	private static final Logger log = LoggerFactory.getLogger(SpringbootReactorApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootReactorApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		Flux<String> names = Flux.just("Andres", "Diego", "Pedro", "Juan", "Bruce")
-				.doOnNext(System.out::println);
+		Flux<String> names = Flux.just("Andres", "Diego", "María", "Pedro", "Juan", "Bruce")
+				.doOnNext(System.out::println)
+				.doOnNext(name -> System.out.println(name.toUpperCase()))
+				.doOnNext(name -> {
+					if(name.isBlank()) {
+						throw new RuntimeException("Nombres no pueden ser vacíos");
+					}
+					System.out.println(name.toLowerCase());
+				});
+		names.subscribe(
+				log::info,
+				error -> log.error(error.getMessage()),
+				() -> log.info("El flujo finalizó correctamente del Observable")
+		);
 	}
 }
