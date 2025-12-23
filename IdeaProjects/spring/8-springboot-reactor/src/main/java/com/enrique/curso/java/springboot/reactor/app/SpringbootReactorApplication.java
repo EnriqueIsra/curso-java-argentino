dbf5d3f854767c9843fc7bf8bd7a7cc15d68c5c6
EnriqueIsra <sirilo99x3@gmail.com>
2025-12-23 11:18:55 -0600

    646. Creando Proyecto con Reactor API

---
+++ IdeaProjects/spring/8-springboot-reactor/src/main/java/com/enrique/curso/java/springboot/reactor/app/SpringbootReactorApplication.java
@@ -0,0 +1,20 @@
+package com.enrique.curso.java.springboot.reactor.app;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
+
+@SpringBootApplication
+public class SpringbootReactorApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootReactorApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Flux<String> names = Flux.just("Andres", "Diego", "Pedro", "Juan", "Bruce")
+				.doOnNext(System.out::println);
+	}
+}
