0527c2810a71c58841b1e6e67c2d0ac62c56468e
EnriqueIsra <sirilo99x3@gmail.com>
2025-12-23 12:54:32 -0600

    648. El Subscriber y Subscription

--- IdeaProjects/spring/8-springboot-reactor/src/main/java/com/enrique/curso/java/springboot/reactor/app/SpringbootReactorApplication.java
+++ IdeaProjects/spring/8-springboot-reactor/src/main/java/com/enrique/curso/java/springboot/reactor/app/SpringbootReactorApplication.java
@@ -1,5 +1,7 @@
 package com.enrique.curso.java.springboot.reactor.app;
 
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -18,6 +20,26 @@ public class SpringbootReactorApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		mapAndFilter();
+	}
+	private void mapAndFilter() {
+		Flux<String> names = Flux.just("Andres", "Diego", "María", "Pedro", "Juan", "Bruce")
+				.map(String::toUpperCase)
+				.doOnNext(name -> {
+					if(name.isBlank()) {
+						throw new RuntimeException("Nombres no pueden ser vacíos");
+					}
+					System.out.println(name.toLowerCase());
+				})
+				;
+
+		names.subscribe(
+				log::info,
+				error -> log.error(error.getMessage()),
+				() -> log.info("El flujo finalizó correctamente del Observable")
+		);
+	}
+	private void subscribeAndOnNext() {
 		Flux<String> names = Flux.just("Andres", "Diego", "María", "Pedro", "Juan", "Bruce")
 				.doOnNext(System.out::println)
 				.doOnNext(name -> System.out.println(name.toUpperCase()))
@@ -32,5 +54,26 @@ public class SpringbootReactorApplication implements CommandLineRunner {
 				error -> log.error(error.getMessage()),
 				() -> log.info("El flujo finalizó correctamente del Observable")
 		);
+//		names.subscribe(new Subscriber<String>() {
+//			@Override
+//			public void onSubscribe(Subscription subscription) {
+//				subscription.request(2);
+//			}
+//
+//			@Override
+//			public void onNext(String string) {
+//				log.info(string);
+//			}
+//
+//			@Override
+//			public void onError(Throwable throwable) {
+//				log.error(throwable.getMessage());
+//			}
+//
+//			@Override
+//			public void onComplete() {
+//				log.info("El flujo finalizó correctamente del Observable");
+//			}
+//		});
 	}
 }
