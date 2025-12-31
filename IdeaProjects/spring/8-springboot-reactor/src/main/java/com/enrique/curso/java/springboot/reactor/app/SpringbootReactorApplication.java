7c8648213de4e066992ac3f67266c5e2ab813082
Enrique Israel Torres Mendoza <enrique.torres@maindsoft.net>
2025-12-31 11:54:27 -0600

    avance clase 650

--- IdeaProjects/spring/8-springboot-reactor/src/main/java/com/enrique/curso/java/springboot/reactor/app/SpringbootReactorApplication.java
+++ IdeaProjects/spring/8-springboot-reactor/src/main/java/com/enrique/curso/java/springboot/reactor/app/SpringbootReactorApplication.java
@@ -15,128 +15,136 @@ import java.time.LocalDateTime;
 @SpringBootApplication
 public class SpringbootReactorApplication implements CommandLineRunner {
 
-	private static final Logger log = LoggerFactory.getLogger(SpringbootReactorApplication.class);
+    private static final Logger log = LoggerFactory.getLogger(SpringbootReactorApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootReactorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootReactorApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		mapAndFilter4User();
-	}
-	private void mapAndFilter4User() {
-		Flux<User> names = Flux.just("Alejandra Sánchez", "Enrique Mendoza", "Santiago Torres", "Sebastian Torres", "Samuel Mendoza")
-				.map(String::trim)
-				.doOnNext(name -> {
-					String[] parts = name.split("\\s+");
+    @Override
+    public void run(String... args) throws Exception {
+        mapAndFilter4User();
+    }
 
-					if (name.isBlank() || parts.length < 2) {
-						throw new RuntimeException("Nombres ni Apellidos no pueden ser vacíos");
-					}
-				})
-				.map(name -> {
-					String[] parts = name.split("\\s+");
-					return new User(parts[0], parts[1]);
-				})
-				.filter(user -> user.getLastname().length() == 7)
-				.doOnNext(System.out::println)
-				.filter(user -> user.getLastname().equalsIgnoreCase("mendoza"))
-				.doOnNext(user -> {
-					if (user.getLastname().isEmpty() || user.getName().isEmpty()) {
-						throw new RuntimeException("Nombres ni Apellidos no pueden ser vacíos");
-					}
-					System.out.println(user.getLastname().length());
-				})
-				.map(user -> {
-					String lastnameUpperCase = user.getLastname().toUpperCase();
-					user.setLastname(lastnameUpperCase);
-					user.setCreatedAt(LocalDateTime.now());
-					return user;
-				})
-				;
+    private void mapAndFilter4User() {
+        Flux<String> names = Flux.just(
+                "Alejandra Sánchez",
+                "Enrique Mendoza",
+                "Santiago Torres",
+                "Sebastian Torres",
+                "Samuel Mendoza");
 
-		names.subscribe(
-				(user) -> log.info(user.toString()),
-				error -> log.error(error.getMessage()),
-				() -> log.info("El flujo finalizó correctamente del Observable")
-		);
-	}
-	private void mapAndFilter3User() {
-		Flux<User> names = Flux.just("Alejandra", "Enrique Mendoza", "Santiago Torres", "Sebastian Torres", "Samuel Mendoza")
-				.map(name -> new User(name.split(" ")[0], name.split(" ")[1]))
-				.filter(user -> user.getLastname().length() == 7)
-				.doOnNext(System.out::println)
-				.filter(user -> user.getLastname().equalsIgnoreCase("mendoza"))
-				.doOnNext(user -> {
-					if (user.getLastname().isEmpty() || user.getName().isEmpty()) {
-						throw new RuntimeException("Nombres ni Apellidos no pueden ser vacíos");
-					}
-					System.out.println(user.getLastname().length());
-				})
-				.map(user -> {
-					String lastnameUpperCase = user.getLastname().toUpperCase();
-					user.setLastname(lastnameUpperCase);
-					user.setCreatedAt(LocalDateTime.now());
-					return user;
-				})
-				;
+        Flux<User> users = names.map(String::trim)
+                .doOnNext(name -> {
+                    String[] parts = name.split("\\s+");
 
-		names.subscribe(
-				(user) -> log.info(user.toString()),
-				error -> log.error(error.getMessage()),
-				() -> log.info("El flujo finalizó correctamente del Observable")
-		);
-	}
-	private void mapAndFilter2Integer() {
-		Flux<Integer> names = Flux.just("Andres", "Diego", "María", "Pedro", "Juan", "Bruce")
-				.map(String::length)
-				.doOnNext(length -> {
-					if(length == 0) {
-						throw new RuntimeException("Nombres no pueden ser vacíos");
-					}
-					System.out.println(length);
-				})
-				;
+                    if (name.isBlank() || parts.length < 2) {
+                        throw new RuntimeException("Nombres ni Apellidos no pueden ser vacíos");
+                    }
+                })
+                .map(name -> {
+                    String[] parts = name.split("\\s+");
+                    return new User(parts[0], parts[1]);
+                })
+                .filter(user -> user.getLastname().length() == 7)
+                .doOnNext(System.out::println)
+                .filter(user -> user.getLastname().equalsIgnoreCase("mendoza"))
+                .doOnNext(user -> {
+                    if (user.getLastname().isEmpty() || user.getName().isEmpty()) {
+                        throw new RuntimeException("Nombres ni Apellidos no pueden ser vacíos");
+                    }
+                    System.out.println(user.getLastname().length());
+                })
+                .map(user -> {
+                    String lastnameUpperCase = user.getLastname().toUpperCase();
+                    user.setLastname(lastnameUpperCase);
+                    user.setCreatedAt(LocalDateTime.now());
+                    return user;
+                });
 
-		names.subscribe(
-				(value) -> log.info(value.toString()),
-				error -> log.error(error.getMessage()),
-				() -> log.info("El flujo finalizó correctamente del Observable")
-		);
-	}
-	private void mapAndFilter() {
-		Flux<String> names = Flux.just("Andres", "Diego", "María", "Pedro", "Juan", "Bruce")
-				.map(String::toUpperCase)
-				.doOnNext(name -> {
-					if(name.isBlank()) {
-						throw new RuntimeException("Nombres no pueden ser vacíos");
-					}
-					System.out.println(name.toLowerCase());
-				})
-				;
+        names.subscribe(System.out::println);
+        users.subscribe(
+                (user) -> log.info(user.toString()),
+                error -> log.error(error.getMessage()),
+                () -> log.info("El flujo finalizó correctamente del Observable")
+        );
+    }
 
-		names.subscribe(
-				log::info,
-				error -> log.error(error.getMessage()),
-				() -> log.info("El flujo finalizó correctamente del Observable")
-		);
-	}
-	private void subscribeAndOnNext() {
-		Flux<String> names = Flux.just("Andres", "Diego", "María", "Pedro", "Juan", "Bruce")
-				.doOnNext(System.out::println)
-				.doOnNext(name -> System.out.println(name.toUpperCase()))
-				.doOnNext(name -> {
-					if(name.isBlank()) {
-						throw new RuntimeException("Nombres no pueden ser vacíos");
-					}
-					System.out.println(name.toLowerCase());
-				});
-		names.subscribe(
-				log::info,
-				error -> log.error(error.getMessage()),
-				() -> log.info("El flujo finalizó correctamente del Observable")
-		);
+    private void mapAndFilter3User() {
+        Flux<User> names = Flux.just("Alejandra", "Enrique Mendoza", "Santiago Torres", "Sebastian Torres", "Samuel Mendoza")
+                .map(name -> new User(name.split(" ")[0], name.split(" ")[1]))
+                .filter(user -> user.getLastname().length() == 7)
+                .doOnNext(System.out::println)
+                .filter(user -> user.getLastname().equalsIgnoreCase("mendoza"))
+                .doOnNext(user -> {
+                    if (user.getLastname().isEmpty() || user.getName().isEmpty()) {
+                        throw new RuntimeException("Nombres ni Apellidos no pueden ser vacíos");
+                    }
+                    System.out.println(user.getLastname().length());
+                })
+                .map(user -> {
+                    String lastnameUpperCase = user.getLastname().toUpperCase();
+                    user.setLastname(lastnameUpperCase);
+                    user.setCreatedAt(LocalDateTime.now());
+                    return user;
+                });
+
+        names.subscribe(
+                (user) -> log.info(user.toString()),
+                error -> log.error(error.getMessage()),
+                () -> log.info("El flujo finalizó correctamente del Observable")
+        );
+    }
+
+    private void mapAndFilter2Integer() {
+        Flux<Integer> names = Flux.just("Andres", "Diego", "María", "Pedro", "Juan", "Bruce")
+                .map(String::length)
+                .doOnNext(length -> {
+                    if (length == 0) {
+                        throw new RuntimeException("Nombres no pueden ser vacíos");
+                    }
+                    System.out.println(length);
+                });
+
+        names.subscribe(
+                (value) -> log.info(value.toString()),
+                error -> log.error(error.getMessage()),
+                () -> log.info("El flujo finalizó correctamente del Observable")
+        );
+    }
+
+    private void mapAndFilter() {
+        Flux<String> names = Flux.just("Andres", "Diego", "María", "Pedro", "Juan", "Bruce")
+                .map(String::toUpperCase)
+                .doOnNext(name -> {
+                    if (name.isBlank()) {
+                        throw new RuntimeException("Nombres no pueden ser vacíos");
+                    }
+                    System.out.println(name.toLowerCase());
+                });
+
+        names.subscribe(
+                log::info,
+                error -> log.error(error.getMessage()),
+                () -> log.info("El flujo finalizó correctamente del Observable")
+        );
+    }
+
+    private void subscribeAndOnNext() {
+        Flux<String> names = Flux.just("Andres", "Diego", "María", "Pedro", "Juan", "Bruce")
+                .doOnNext(System.out::println)
+                .doOnNext(name -> System.out.println(name.toUpperCase()))
+                .doOnNext(name -> {
+                    if (name.isBlank()) {
+                        throw new RuntimeException("Nombres no pueden ser vacíos");
+                    }
+                    System.out.println(name.toLowerCase());
+                });
+        names.subscribe(
+                log::info,
+                error -> log.error(error.getMessage()),
+                () -> log.info("El flujo finalizó correctamente del Observable")
+        );
 //		names.subscribe(new Subscriber<String>() {
 //			@Override
 //			public void onSubscribe(Subscription subscription) {
@@ -158,5 +166,5 @@ public class SpringbootReactorApplication implements CommandLineRunner {
 //				log.info("El flujo finalizó correctamente del Observable");
 //			}
 //		});
-	}
+    }
 }
