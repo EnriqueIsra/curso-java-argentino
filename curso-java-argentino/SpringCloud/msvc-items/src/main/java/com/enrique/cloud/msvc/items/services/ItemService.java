f4b3187feb4dd75551b19ba9394c5a3ffae1c428
Enrique Israel Torres Mendoza <enrique.torres@maindsoft.net>
2026-08-20 12:59:04 -0600

    676. Implementando Service con Feign Client Http

--- curso-java-argentino/SpringCloud/msvc-items/src/main/java/com/enrique/cloud/msvc/items/services/ItemService.java
+++ curso-java-argentino/SpringCloud/msvc-items/src/main/java/com/enrique/cloud/msvc/items/services/ItemService.java
@@ -4,11 +4,10 @@ import java.util.List;
 import java.util.Optional;
 
 import com.enrique.cloud.msvc.items.models.Item;
-import com.enrique.cloud.msvc.items.models.Product;
 
 public interface ItemService {
 
     List<Item> findAll();
 
-    Optional<Product> findById(Long id);
+    Optional<Item> findById(Long id);
 }
