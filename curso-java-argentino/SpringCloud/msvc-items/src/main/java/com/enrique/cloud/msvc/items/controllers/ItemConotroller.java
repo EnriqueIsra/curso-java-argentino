f211a7f89e958dc70226be2a40db20a97f21a675
Enrique Israel Torres Mendoza <enrique.torres@maindsoft.net>
2026-08-20 13:17:31 -0600

    677. Creando controlador en items

---
+++ curso-java-argentino/SpringCloud/msvc-items/src/main/java/com/enrique/cloud/msvc/items/controllers/ItemConotroller.java
@@ -0,0 +1,41 @@
+package com.enrique.cloud.msvc.items.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.enrique.cloud.msvc.items.models.Item;
+import com.enrique.cloud.msvc.items.services.ItemService;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class ItemConotroller {
+
+    private final ItemService service;
+
+    public ItemConotroller(ItemService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Item> list() {
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}") 
+    public ResponseEntity<Item> details(@PathVariable Long id){
+        Optional<Item> itemOptional = service.findById(id);
+        if (itemOptional.isPresent()){
+            return ResponseEntity.ok(itemOptional.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+    
+
+}
