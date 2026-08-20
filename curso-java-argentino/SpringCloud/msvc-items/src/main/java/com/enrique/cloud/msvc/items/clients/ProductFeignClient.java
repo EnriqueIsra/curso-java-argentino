4c47fbd3437d4c1667992fe3a171643282ebe24a
Enrique Israel Torres Mendoza <enrique.torres@maindsoft.net>
2026-08-20 12:38:51 -0600

    675. Implementando Client Http con OpenFeign

---
+++ curso-java-argentino/SpringCloud/msvc-items/src/main/java/com/enrique/cloud/msvc/items/clients/ProductFeignClient.java
@@ -0,0 +1,19 @@
+package com.enrique.cloud.msvc.items.clients;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.enrique.cloud.msvc.items.models.Product;
+
+@FeignClient(url = "localhost:8001")
+public interface ProductFeignClient {
+
+    @GetMapping
+    List<Product> findAll();
+
+    @GetMapping("/{id}")
+    Product details(@PathVariable Long id);
+}
