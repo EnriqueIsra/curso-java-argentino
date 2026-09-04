3f3e016a6a9dcc10272f73ff24428437ea0ec651
Enrique Israel Torres Mendoza <enrique.torres@maindsoft.net>
2026-09-04 10:27:06 -0600

    692. Creando servicio Spring Cloud Gateway

---
+++ curso-java-argentino/SpringCloud/msvc-gatewat-server/src/main/java/com/enrique/springcloud/app/gateway/msvc_gatewat_server/MsvcGatewatServerApplication.java
@@ -0,0 +1,13 @@
+package com.enrique.springcloud.app.gateway.msvc_gatewat_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcGatewatServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcGatewatServerApplication.class, args);
+	}
+
+}
---
+++ curso-java-argentino/SpringCloud/msvc-gatewat-server/src/test/java/com/enrique/springcloud/app/gateway/msvc_gatewat_server/MsvcGatewatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enrique.springcloud.app.gateway.msvc_gatewat_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcGatewatServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
