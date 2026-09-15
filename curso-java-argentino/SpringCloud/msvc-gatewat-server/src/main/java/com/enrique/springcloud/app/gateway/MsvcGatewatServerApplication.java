e910dbcbad2736d013591689454e704762b7c2ad
Enrique Israel Torres Mendoza <enrique.torres@maindsoft.net>
2026-09-15 09:32:30 -0600

    694. Implementando filtros globales pre y post

--- curso-java-argentino/SpringCloud/msvc-gatewat-server/src/main/java/com/enrique/springcloud/app/gateway/MsvcGatewatServerApplication.java
+++ curso-java-argentino/SpringCloud/msvc-gatewat-server/src/main/java/com/enrique/springcloud/app/gateway/MsvcGatewatServerApplication.java
@@ -1,4 +1,4 @@
-package com.enrique.springcloud.app.gateway.msvc_gatewat_server;
+package com.enrique.springcloud.app.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
