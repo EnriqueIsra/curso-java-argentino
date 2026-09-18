f6095204b8714d76d2545bdc875bc452004aec8d
Enrique Israel Torres Mendoza <enrique.torres@maindsoft.net>
2026-09-18 14:47:30 -0600

    696. Modificando el request en el filtro pre. (Curso usa una version mas vieja, por lo cual se ajusto con mutadedExchange para lograr el resultado esperado como en el curso)

--- curso-java-argentino/SpringCloud/msvc-gatewat-server/src/main/java/com/enrique/springcloud/app/gateway/filters/SampleGlobalFilter.java
+++ curso-java-argentino/SpringCloud/msvc-gatewat-server/src/main/java/com/enrique/springcloud/app/gateway/filters/SampleGlobalFilter.java
@@ -1,30 +1,48 @@
 package com.enrique.springcloud.app.gateway.filters;
 
+
+import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseCookie;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
-
 import reactor.core.publisher.Mono;
 
-@Component 
-public class SampleGlobalFilter implements GlobalFilter {
+@Component
+public class SampleGlobalFilter implements GlobalFilter, Ordered {
+   private final Logger logger = LoggerFactory.getLogger(SampleGlobalFilter.class);
 
-    private final Logger logger = LoggerFactory.getLogger(SampleGlobalFilter.class);
-    @Override
-    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        logger.info("Ejecuntando el filtro antes del request PRE");
+   public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+      this.logger.info("ejecutando el filtro antes del request PRE");
+      ServerHttpRequest request = exchange.getRequest().mutate()
+        .headers(h -> h.add("token", "abcdefg")).build();
 
-        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-            logger.info("Ejecutando filtro POST response");
+        ServerWebExchange mutatedExchange = exchange.mutate().request(request).build();
 
-            exchange.getResponse().getCookies().add("color", ResponseCookie.from("color", "red").build());
-            exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
-        }));
-    }
+      return chain.filter(mutatedExchange).then(Mono.fromRunnable(() -> {
+         this.logger.info("ejecutando filtro POST response");
+         String token = mutatedExchange.getRequest().getHeaders().getFirst("token");
+         if (token != null) {
+            this.logger.info("token: " + token);
+            mutatedExchange.getResponse().getHeaders().add("token", token);
+         }
 
+         Optional.ofNullable(mutatedExchange.getRequest().getHeaders().getFirst("token")).ifPresent((value) -> {
+            this.logger.info("token2: " + value);
+            mutatedExchange.getResponse().getHeaders().add("token2", value);
+         });
+         mutatedExchange.getResponse().getCookies().add("color", ResponseCookie.from("color", "red").build());
+         mutatedExchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
+      }));
+   }
+
+   public int getOrder() {
+      return 100;
+   }
 }
